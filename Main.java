0580a16f86e7358b9ac8a5db2585fec2f809745c
Pepel777 <pepel77777@gmail.com>
2018-04-03 20:57:38 +0300

    '2'

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Moto myMoto = new Moto("BMV");
-        Moto myMoto2 = new Moto("M72", 1953);
-        myMoto.drive();
-        myMoto2.drive();
-    }
-}
-
